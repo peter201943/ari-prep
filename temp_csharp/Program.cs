d00d34466dd39a198a0f366e8ed5a73f0d22ea0c
pjm349@drexel.edu <pjm349@drexel.edu>
2020-04-01 11:15:01 -0400

    Cleanup Folder, but need to rename

--- temp_csharp/Program.cs
+++ temp_csharp/Program.cs
@@ -4,10 +4,15 @@ class Program
 {
     static void Main()
     {
+        /* 
         Console.WriteLine("Hello World! 1");
         Console.WriteLine("Hello World! 2");
         Console.WriteLine("Hello World! 3");
         Console.WriteLine("Hello World! 4");
         Console.WriteLine("Hello World! 5");
+        */
+        Console.WriteLine("word");
+        Console.ReadLine();
+        Console.Clear();
     }
 }

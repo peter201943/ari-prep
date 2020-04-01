46becdb3506f0b80c3ee201e89ba10e8e5106dd1
pjm349@drexel.edu <pjm349@drexel.edu>
2020-04-01 12:47:15 -0400

    Cleanup and Renaming

---
+++ temp_csharp/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_practice
+{ 
+/// <summary>
+/// Remake of Java Quiz System
+/// Meant to be abstract menus - can be read from console, gui, etc.
+/// </summary>
+class Menu
+{
+    protected String _author;
+    protected String _time;
+    protected String _content;
+    protected List<Menu> _children;
+    protected Menu _parent;
+}
+}
